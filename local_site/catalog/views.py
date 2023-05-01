dfb156a9a28ceaefddb0bc5082cc23acdfa56c85
Dmitry <k1791@mail.ru>
2023-05-01 19:58:29 +0500

    step4 index

--- local_site/catalog/views.py
+++ local_site/catalog/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def index (request):
+    return render (request, 'catalog/index.html')
+
+def contacts (request):
+    return HttpResponse ("Контакты:")
