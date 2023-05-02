c29e0af3efd51d8a0dc982208709dab76901befd
Dmitry <k1791@mail.ru>
2023-05-02 20:57:54 +0500

    возврат значений POST запрос

--- local_site/catalog/views.py
+++ local_site/catalog/views.py
@@ -3,7 +3,12 @@ from django.shortcuts import render
 
 # Create your views here.
 def index (request):
-    return render (request, 'catalog/index.html')
+      return render (request, 'catalog/index.html')
 
 def contacts (request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        print(f'User {name} with phone {phone}- send message: {message}')
     return render (request, 'catalog/contacts.html')
